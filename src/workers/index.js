dc9c872f34ef03a38a20fd2cdcffdfc85aa70bad
Svetlana Teneva <steneva99@gmail.com>
2020-04-14 05:00:20 +0300

    Add support for service worker.

--- src/workers/index.js
+++ src/workers/index.js
@@ -1,30 +1,63 @@
 /* eslint-disable multiline-ternary */
 import path from 'path';
-import { getWorkerConstructorName } from '../utils';
 
-const getWorker = (file, content, options) => {
+export const WORKER_TYPES = {
+  DEDICATED: 'dedicated',
+  SHARED: 'shared',
+  SERVICE: 'service',
+};
+
+const getWorker = (workerFilePath, content, options) => {
   const publicPath = options.publicPath
     ? JSON.stringify(options.publicPath)
     : '__webpack_public_path__';
 
-  const publicWorkerPath = `${publicPath} + ${JSON.stringify(file)}`;
+  const publicWorkerPath = `${publicPath} + ${JSON.stringify(workerFilePath)}`;
 
   if (options.inline) {
-    const InlineWorkerPath = JSON.stringify(
-      `!!${path.join(__dirname, 'InlineWorker.js')}`
+    const fallbackWorkerPath = options.fallback ? publicWorkerPath : 'null';
+
+    const inlineWorkerName = options.name || 'InlineWorker.js';
+    const inlineWorkerCreateScript = getInlineWorkerCreateScript(
+      inlineWorkerName,
+      content,
+      fallbackWorkerPath
     );
-
-    const fallbackWorkerPath =
-      options.fallback === false ? 'null' : publicWorkerPath;
-
-    return `require(${InlineWorkerPath})(${JSON.stringify(
-      content
-    )}, ${fallbackWorkerPath})`;
+    return inlineWorkerCreateScript;
   }
 
-  const workerConstructorName = getWorkerConstructorName(options.type);
+  const workerCreateScript = getWorkerCreateScript(
+    options.type,
+    publicWorkerPath
+  );
+  return workerCreateScript;
+};
 
-  return `new ${workerConstructorName}(${publicWorkerPath})`;
+const getInlineWorkerCreateScript = (name, content, fallbackWorkerPath) => {
+  const inlineWorkerPath = JSON.stringify(`!!${path.join(__dirname, name)}`);
+
+  return `require(${inlineWorkerPath})(${JSON.stringify(
+    content
+  )}, ${fallbackWorkerPath})`;
+};
+
+const getWorkerCreateScript = (type, publicWorkerPath) => {
+  switch (type) {
+    case WORKER_TYPES.SHARED: {
+      return `new SharedWorker(${publicWorkerPath})`;
+    }
+    case WORKER_TYPES.SERVICE: {
+      return `('serviceWorker' in navigator)
+      ? navigator.serviceWorker.register(${publicWorkerPath}, options)
+      : Promise.reject(
+        new Error('navigator.serviceWorker is not supported in this browser')
+      );`;
+    }
+    case WORKER_TYPES.DEDICATED:
+    default: {
+      return `new Worker(${publicWorkerPath})`;
+    }
+  }
 };
 
 export default getWorker;
